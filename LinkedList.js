7584d69f9b5f9cd1de831a392bde16148b802f39
Curtis Laurence Chadwell <>
2021-04-29 02:36:53 +0000

    Initial commit

---
+++ LinkedList.js
@@ -0,0 +1,90 @@
+class LinkedList {
+    constructor(headNode){
+      this.head = headNode || null
+      this.size = headNode ? 1 : 0
+      this.tail = 'This is tail'
+      this.next = null
+    }
+
+    size(){
+     let count = 0
+     let node = this.head
+     while (node){
+       count ++
+       node = node.next
+     }
+    }
+
+    clear(){
+      this.head = null
+    }
+
+    getlast(){
+
+    }
+
+    getFirst(){}
+
+
+}
+
+export default LinkedList
+
+
+//append(value)
+    //create a new node
+    //if no head or tail
+      //head = the new node
+      //tail = new node
+    //else
+      //connect new node to tail
+      //reassign the tail
+    //increament the size
+
+
+
+
+//     class LinkedList {
+//     constructor() {
+//         this.size = 0;
+//         this.head = null;
+//         this.tail = null;
+//     }
+
+//     // Add to the front of the linked list.
+//     prepend(value) {
+//         this.size += 1
+
+//         // Create a new node
+//         const node = Node(value, this.head)
+//         this.head = node;
+
+//         if (!this.tail) this.tail = node;
+//         return this
+//     }
+
+//     append(value) {
+//         // if there is no head or tail
+//             // head = the new node
+//             // tail = new node
+//         //  else
+//             // Create a new node with new value
+//             // Connect new Node to tail
+//             // Reassign the tail
+//         // increase size by 1
+//     }
+
+//     fromArray() {}
+
+//     toArray() {}
+
+//     delete() {}
+
+//     deleteHead() {}
+
+//     deleteTail() {}
+
+//     includes() {}
+
+//     find() {}
+// }
---
+++ Node.js
@@ -0,0 +1,45 @@
+//Linked List
+//A linear data structure
+
+//Node - a container of data
+//Can hold string, objs, arrays, numbers, boolean, etc
+
+//Practical example
+//the play back and forward buttons on a spotify 
+
+
+//Doubly liked List
+//two pointers
+//first next
+
+//How does a linked list work?
+
+//-always has a HEAD = entry point
+
+//node = data + reference to the next node
+
+//nodes can live eveyrwhere and grow dynamically
+
+//Requires two classes node and list
+
+//a node class
+class Node{
+  constructor(el){
+    this.el = el
+    this.next = 'Tail'
+    // this.prev = null //only if doubly linked list
+  }
+}
+
+export default Node
+
+
+
+
+// class LinkedList {
+//   constructor(){
+
+//   }
+// }
+
+
